b1fc9254c087003f0d6005eb6f675b51275ecf47
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-04-10 16:19:18 -0400

    Adopted for graph API development

--- Iris-example/app.py
+++ Iris-example/app.py
@@ -1,42 +1,28 @@
-# Ref: https://towardsdatascience.com/deploy-your-machine-learning-model-as-a-rest-api-4fe96bf8ddcc
-
 import os
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
-from model.Train import train_model
-from sklearn.externals import joblib
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
-if not os.path.isfile('iris-model.model'):
-    train_model()
+if not os.path.isfile('dev.speakai.api.json'):
+    exit('No analytics data available !')
 
-model = joblib.load('iris-model.model')
+with open('dev.speakai.api.json') as f:
+  data = json.load(f)
 
-class MakePrediction(Resource):
+class Generator(Resource):
     @staticmethod
-    def post():
-        posted_data = request.get_json()
-        sepal_length = posted_data['sepal_length']
-        sepal_width = posted_data['sepal_width']
-        petal_length = posted_data['petal_length']
-        petal_width = posted_data['petal_width']
+    def get():
 
-        prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
-
-        if prediction == 0:
-            predicted_class = 'Iris-setosa'
-        elif prediction == 1:
-            predicted_class = 'Iris-versicolor'
-        else:
-            predicted_class = 'Iris-virginica'
-
+        graphSpec = data
         return jsonify({
-            'Prediction': predicted_class
+            'specs': graphSpec
         })
 
-api.add_resource(MakePrediction, '/predict')
+
+api.add_resource(Generator, '/graph')
 
 if __name__ == '__main__':
     app.run(debug=True)
