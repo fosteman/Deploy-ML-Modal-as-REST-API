d40d4bafcad5079e161551b56992cc91b4be0cde
Timofei Shchepkin <dgrz5.0@gmail.com>
2020-04-10 18:19:27 -0400

    Figure out date range

--- Iris-example/app.py
+++ Iris-example/app.py
@@ -2,6 +2,10 @@ import os
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 import json
+import altair as alt
+import numpy as np
+import pandas as pd
+from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,15 +14,32 @@ if not os.path.isfile('dev.speakai.api.json'):
     exit('No analytics data available !')
 
 with open('dev.speakai.api.json') as f:
-  data = json.load(f)
+    data = json.load(f)
+
+# Learns the earliest and nearest dates for processed analytics
+def figure_out_daterange(analytics):
+    earliestDate = datetime.now()
+    presentDate = datetime.now()
+    for upload in analytics[0]:
+        if (np.datetime64(upload['createdAt'], 'D') < np.datetime64(earliestDate, 'D')):
+            earliestDate = upload['createdAt']
+            # print(upload['createdAt'], ' is earlier than ', earliestDate)
+
+    return np.arange(np.datetime64(earliestDate), np.datetime64(presentDate), dtype='datetime64[D]')
 
 class Generator(Resource):
     @staticmethod
     def get():
+        # Obtain analytics data
+        analyticsData = data[0:9]
 
-        graphSpec = data
+        # Generate Graph Specs
+        ## figure out date range
+        figure_out_daterange(analyticsData)
+
         return jsonify({
-            'specs': graphSpec
+            'data': analyticsData[0],
+            'specs': 'Vega Graph Specs! '
         })
 
 
@@ -26,3 +47,5 @@ api.add_resource(Generator, '/graph')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
